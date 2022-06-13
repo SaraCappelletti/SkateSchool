c9786dec6b95dd4a8697f9b9c4f227e4db089091
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2022-06-13 11:07:40 +0200

    Add new query

---
+++ Query iniziali/dbo.Iscritto.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Iscritto] ([CodiceIscritto], [Nome], [Cognome], [Eta], [Telefono], [Livello]) VALUES (1, N'Sara', N'Cappelletti', 20, N'3356894488', N'base')
+INSERT INTO [dbo].[Iscritto] ([CodiceIscritto], [Nome], [Cognome], [Eta], [Telefono], [Livello]) VALUES (2, N'Gabriele', N'Neri', 12, N'3379757733', N'avanzato')
+INSERT INTO [dbo].[Iscritto] ([CodiceIscritto], [Nome], [Cognome], [Eta], [Telefono], [Livello]) VALUES (3, N'Claudia', N'Castellucci', 30, N'3378649528', N'adulti avanzato')
